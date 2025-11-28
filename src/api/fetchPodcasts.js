00be2f737aba31e49328c53446a29ff36e0bd04b
Tashreeq Adams <tashreeqadams8@gmail.com>
2025-11-28 13:45:35 +0200

    Fetch API added

---
+++ src/api/fetchPodcasts.js
@@ -0,0 +1,24 @@
+/**
+ * @function fetchPodcasts
+ * Asynchronously fetches podcast data from the remote API and updates state accordingly.
+ * Handles loading, error, and successful data response via provided state setters.
+ *
+ * @param {Function} setPodcasts - State setter function to update the podcasts array.
+ * @param {Function} setError - State setter function to update the error message (string).
+ * @param {Function} setLoading - State setter function to toggle the loading state (boolean).
+ *
+ * @returns {Promise<void>} A promise that resolves when the fetch process completes.
+ *
+ **/
+export async function fetchPodcasts(setPodcast, setError, setLoading) {
+  try {
+    const res = await fetch("https://podcast-api.netlify.app/");
+    if (!res.ok) throw new Error(`${res.status}`);
+    const data = await res.json();
+    setPodcast(data);
+  } catch (err) {
+    console.err("Failed to fetch", err);
+  } finally {
+    setLoading = false;
+  }
+}
