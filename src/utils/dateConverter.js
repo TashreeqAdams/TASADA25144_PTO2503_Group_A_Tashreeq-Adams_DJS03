d0ec031e9a8da73612f0972dfa4aa7134e5a82e9
Tashreeq Adams <tashreeqadams8@gmail.com>
2025-11-30 19:17:12 +0200

    Added a JSDoc comment

--- src/utils/dateConverter.js
+++ src/utils/dateConverter.js
@@ -1,3 +1,9 @@
+/**
+ * Converts an ISO date string into a human-readable "Updated {date}" format.
+ *
+ * @param {string|number|Date} isoString - An ISO timestamp or any valid date input for `Date()`.
+ * @returns {string} A formatted string.
+ */
 export function daysAgo(isoString) {
   const pastDate = new Date(isoString);
 
